15e58834a55241a59b4ac9addd1d5e07c12c4db2
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-08-30 18:50:20 +0000

    - Initial commit

---
+++ lib/manwar.pm
@@ -0,0 +1,78 @@
+package manwar;
+
+use strict; use warnings;
+use Data::Dumper;
+use JSON;
+use Dancer2;
+use Dancer2::FileUtils qw(path read_file_content);
+
+=head1 NAME
+
+Dancer2 App - manwar.org
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 AUTHOR
+
+Mohammad S Anwar, C<< <mohammad.anwar at yahoo.com> >>
+
+=cut
+
+$manwar::VERSION   = '0.01';
+$manwar::AUTHORITY = 'cpan:MANWAR';
+
+get '/' => sub {
+    template 'stats';
+};
+
+get '/cpan-release' => sub {
+    template 'stats';
+};
+
+get '/stats/monthly' => sub {
+
+    my $file = Dancer2::FileUtils::path(setting('appdir'), 'public', 'stats', 'monthly.json');
+    my $data = Dancer2::FileUtils::read_file_content($file);
+    my $stat = JSON->new->decode($data);
+
+    content_type 'application/json';
+    return to_json($stat);
+};
+
+get '/stats/weekly' => sub {
+
+    my $file = Dancer2::FileUtils::path(setting('appdir'), 'public', 'stats', 'weekly.json');
+    my $data = Dancer2::FileUtils::read_file_content($file);
+    my $stat = JSON->new->decode($data);
+
+    content_type 'application/json';
+    return to_json($stat);
+};
+
+get '/stats/daily' => sub {
+
+    my $file = Dancer2::FileUtils::path(setting('appdir'), 'public', 'stats', 'daily.json');
+    my $data = Dancer2::FileUtils::read_file_content($file);
+    my $stat = JSON->new->decode($data);
+
+   content_type 'application/json';
+   return to_json($stat);
+};
+
+get '/my-cpan' => sub {
+    template 'my-cpan';
+};
+
+get '/my-cpan/stat' => sub {
+
+    my $file = Dancer2::FileUtils::path(setting('appdir'), 'public', 'stats', 'my-cpan.json');
+    my $data = Dancer2::FileUtils::read_file_content($file);
+    my $stat = JSON->new->decode($data);
+
+    content_type 'application/json';
+    return to_json($stat);
+};
+
+1;
---
+++ t/000_load.t
@@ -0,0 +1,6 @@
+#!perl
+
+use Test::More tests => 1;
+
+BEGIN { use_ok( 'manwar' ) || print "Bail out!"; }
+diag( "Testing Dancer app manwar $manwar::VERSION, Perl $], $^X" );
