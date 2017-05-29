eff023150f80050e8d52f3fc242e5d38fd2c68a0
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2017-05-29 13:22:56 +0100

    - Updated manwar.pm and images subfolder.

--- lib/manwar.pm
+++ lib/manwar.pm
@@ -4,7 +4,7 @@ use strict; use warnings;
 use Data::Dumper;
 use JSON;
 use MIME::Lite;
-use Dancer2;
+use Dancer2 appname => 'manwar';
 use Dancer2::FileUtils qw(path read_file_content);
 
 =head1 NAME
@@ -13,7 +13,7 @@ Dancer2 App - manwar.org
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =head1 AUTHOR
 
@@ -21,18 +21,76 @@ Mohammad S Anwar, C<< <mohammad.anwar at yahoo.com> >>
 
 =cut
 
-$manwar::VERSION   = '0.16';
+$manwar::VERSION   = '0.17';
 $manwar::AUTHORITY = 'cpan:MANWAR';
 
-get '/contact-us' => sub {
-    template 'contact-us';
+get '/' => sub {
+
+    my $file = read_file_content(path(setting('appdir'), 'public', 'stats', 'cpan-recent.json'));
+    my $data = JSON->new->allow_nonref->utf8(1)->decode($file);
+
+    template 'index' => $data;
 };
 
-post '/contact-us' => sub {
-    my $name    = params->{'name'};
-    my $email   = params->{'email'};
-    my $subject = params->{'subject'};
-    my $message = params->{'message'};
+get '/stats/:type' => sub {
+
+    my $type = params->{type};
+    my $file = sprintf("%s.json", $type);
+
+    return send_data(path(setting('appdir'), 'public', 'stats', $file));
+};
+
+get '/pullrequest/:year' => sub {
+
+    my $year = params->{year};
+    my $file = sprintf("pr-%d.json", $year);
+    return send_data(path(setting('appdir'), 'public', 'stats', $file));
+};
+
+get '/pullrequest-challenge/:year' => sub {
+
+    my $year = params->{year};
+    my $file = sprintf("prc-%d.json", $year);
+    return send_data(path(setting('appdir'), 'public', 'stats', $file));
+};
+
+get '/git-commits/:year' => sub {
+
+    my $year = params->{year};
+    my $file = sprintf("gc-%d.json", $year);
+    return send_data(path(setting('appdir'), 'public', 'stats', $file));
+};
+
+get '/cpan-uploaders/:limit' => sub {
+
+    my $limit = params->{limit};
+    return send_data(path(setting('appdir'), 'public', 'stats', 'cpan-uploaders.json'));
+};
+
+get '/neocpan-uploaders/:limit' => sub {
+
+    my $limit = params->{limit};
+    return send_data(path(setting('appdir'), 'public', 'stats', 'neocpan-uploaders.json'));
+};
+
+get '/adopted-distributions' => sub {
+
+    return send_data(path(setting('appdir'), 'public', 'stats', 'adopted-distributions.json'));
+};
+
+get '/personal-distributions/:start/:end' => sub {
+
+    my $start = params->{start};
+    my $end   = params->{end};
+    my $file  = sprintf("personal-distributions-%d-%d.json", $start, $end);
+    return send_data(path(setting('appdir'), 'public', 'stats', $file));
+};
+
+post '/contact' => sub {
+    my $name    = params->{'uname'};
+    my $email   = params->{'uemail'};
+    my $subject = params->{'usubject'};
+    my $message = params->{'umessage'};
 
     my $status;
     eval {
@@ -50,110 +108,14 @@ post '/contact-us' => sub {
         $status = 'Unable to submit the request, please try again.';
     }
 
-    template 'contact-us' => {
-        status => $status
-    };
+    return $status;
 };
 
-get '/' => sub {
-    template 'stats';
-};
-
-get '/cpan-regulars' => sub {
-    template 'stats';
-};
-
-get '/stats/monthly' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'monthly.json'));
-};
-
-get '/stats/weekly' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'weekly.json'));
-};
-
-get '/stats/daily' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'daily.json'));
-};
-
-get '/cpan-uploaders' => sub {
-    template 'cpan-uploaders';
-};
-
-get '/cpan-uploaders/20' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'cpan-uploaders.json'));
-};
-
-get '/neocpan-uploaders/20' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'neocpan-uploaders.json'));
-};
-
-get '/my-distributions' => sub {
-    template 'my-distributions';
-};
-
-get '/my-distributions-1' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'my-distributions-1.json'));
-};
-
-get '/my-distributions-2' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'my-distributions-2.json'));
-};
-
-get '/my-adopted-cpan' => sub {
-    template 'my-adopted-cpan';
-};
-
-get '/my-adopted-cpan/stat' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'my-adopted-cpan.json'));
-};
-
-get '/my-pullrequest' => sub {
-    template 'my-pullrequest';
-};
-
-get '/my-pullrequest/stat' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'my-pullrequest.json'));
-};
-
-get '/my-prc/stat' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'my-prc.json'));
-};
-
-get '/my-gitcommits' => sub {
-    template 'my-gitcommits';
-};
-
-get '/my-gitcommits/2014-16' => sub {
-
-    return send_data(path(setting('appdir'), 'public', 'stats', 'my-gitcommits.json'));
-};
-
-get '/my-job' => sub {
-    template 'my-job';
-};
-
-get '/my-activities' => sub {
-    template 'my-activities';
-};
-
-get '/my-latest-news' => sub {
-    template 'my-latest-news';
-};
-
+=head
 get '/my-reading-links' => sub {
     template 'my-reading-links';
 };
-
+=cut
 #
 #
 # LOCAL METHODS
