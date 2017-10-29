6aae490449eb50d525e80e862f9545135d0d3d98
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2017-10-29 11:48:37 +0000

    - Removed 'I' and 'l' from the character sets.

--- public/javascripts/manwar.js
+++ public/javascripts/manwar.js
@@ -21,7 +21,7 @@ var chart_data = {
 
 function get_captcha() {
 
-    var alpha = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    var alpha = new Array('A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
     var a = alpha[Math.floor(Math.random() * alpha.length)];
     var b = alpha[Math.floor(Math.random() * alpha.length)];
     var c = alpha[Math.floor(Math.random() * alpha.length)];
