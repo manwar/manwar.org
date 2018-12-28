955cdd56bdeb6a24347680accd9bfce7a92ad5a2
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-12-28 01:58:46 +0000

    - Moved "PR Tracker" to "GitHub Pull Request" section.

--- public/javascripts/manwar.js
+++ public/javascripts/manwar.js
@@ -1,7 +1,6 @@
 $(function() {
     $("#pullrequest_24").click();
     $("#ds").click();
-    $("#pr_tracker_top30").click();
     $("#pr_summary").click();
     $("#prc_summary").click();
     $("#hd_10").click();
